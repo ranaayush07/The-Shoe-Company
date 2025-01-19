366d4134365a7a21da1b65d8c56ffebc5334e769
ayush <ayush885rana@gmail.com>
2025-01-19 16:09:43 +0530

    created mobile menu and add functionality using javascript and created javascript file

---
+++ src/index.js
@@ -0,0 +1,14 @@
+'Use Strict'
+const mobileMenu = document.querySelector(".menu")
+const crossIcon  = document.querySelector(".arrow")
+const hamburgerMenu = document.querySelector(".hamburger")
+hamburgerMenu.addEventListener("click", (e)=>{
+  mobileMenu.classList.add("right-[20px]")
+  hamburgerMenu.classList.add("hidden")
+ 
+})
+crossIcon.addEventListener("click", (e)=>{
+    mobileMenu.classList.remove("right-[20px]")
+    hamburgerMenu.classList.remove("hidden")
+    console.log("arrow pressed")
+})
